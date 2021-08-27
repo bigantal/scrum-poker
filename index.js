40ba5df9572192bdb4796fd7bc7f6fd886f02b27
Gabor Horvath <gabor.horvath@verint.com>
2021-08-27 14:01:24 +0200

    card show/hide comes from central

--- index.js
+++ index.js
@@ -7,24 +7,27 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-// io.on('connection', (socket) => {
-//   socket.on('chat message', msg => {
-//     io.emit('chat message', msg);
-//   });
-// });
-
-let store = [];
+let currentState = {
+  history: [],
+  cardsShown: false
+}
 
 io.on('connection', (socket) => {
   console.log('a user connected');
-  socket.emit('chat history', store);
+  socket.emit('welcome', currentState);
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
   socket.on('chat message', msg => {
     console.log('chat message');
-    store.push(msg);
+    currentState.history.push(msg);
     io.emit('chat message', msg);
+    io.emit('view cards shown', currentState.cardsShown);
+  });
+  socket.on('set cards shown', msg => {
+    console.log('set cards shown', msg);
+    currentState.cardsShown = msg;
+    io.emit('view cards shown', currentState.cardsShown);
   });
 });
 
